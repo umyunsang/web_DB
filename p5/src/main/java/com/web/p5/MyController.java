2f15d5528fbb7cc2048f68b87cf2985835816be4
UmYunSang <dbstkd5865@gmail.com>
2024-05-17 18:38:00 +0900

    basic HTML

--- p5/src/main/java/com/web/p5/MyController.java
+++ p5/src/main/java/com/web/p5/MyController.java
@@ -1,10 +1,13 @@
 package com.web.p5;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
@@ -23,5 +26,27 @@ public class MyController {
         List<product> arr = prep.findAll();
         mo.addAttribute("arr", arr);
         return "productList";  
+    }
+    @GetMapping("/iam")
+    public String iam() {
+    	return "iam";
+    }
+    
+    @PostMapping("/iam/answer")
+    public String iamAnswer(@RequestParam("age") String age,
+    		@RequestParam("gender") String gender,
+    		@RequestParam("foods") String foods,
+    		@RequestParam("face") String face,
+    		@RequestParam("grade") String grade,
+    		@RequestParam("head") String head,
+    		@RequestParam("promote") String promote, Model mo) {
+    	mo.addAttribute("age", age);
+    	mo.addAttribute("gender", gender);
+    	mo.addAttribute("foods", foods);
+    	mo.addAttribute("face", face);
+    	mo.addAttribute("grade", grade);
+    	mo.addAttribute("head", head);
+    	mo.addAttribute("promote", promote);
+    	return "iamAnswer";
     }
 }
